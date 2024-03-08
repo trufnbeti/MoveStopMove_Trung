7f2e497375b13099ce305eb3656fd3460443187b
trufnbeti <trungboy80@gmail.com>
2024-03-08 18:03:51 +0700

    +todo UI Weapon

---
+++ Assets/_Game/ScriptableObjects/WeaponData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "ScriptableObjects/WeaponData", order = 1)]
+public class WeaponData : ScriptableObject
+{
+	[SerializeField] private List<WeaponItem> weaponItems;
+	
+	// public WeaponItem GetWeaponItem(WeaponType weaponType) {
+	// 	// return weaponItems.Single(q => q.type == weaponType);
+	// }
+
+	public WeaponType NextType(WeaponType weaponType) {
+		int index = weaponItems.FindIndex(q => q.type == weaponType);
+		index = index + 1 >= weaponItems.Count ? 0 : index + 1;
+		return weaponItems[index].type;
+	}
+
+	public WeaponType PrevType(WeaponType weaponType) {
+		int index = weaponItems.FindIndex(q => q.type == weaponType);
+		index = index - 1 < 0 ? weaponItems.Count - 1 : index - 1;
+		return weaponItems[index].type;
+	}
+}
+
+[System.Serializable]
+public class WeaponItem {
+	public string name;
+	public WeaponType type;
+	public int cost;
+}
