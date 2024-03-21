12b33f9d5a8a4705b7e130cb949b3e3d92233c1b
trufnbeti <trungboy80@gmail.com>
2024-03-21 09:52:18 +0700

    + Sound weapon hit

--- Assets/_Game/Scripts/GamePlay/Character/Character.cs
+++ Assets/_Game/Scripts/GamePlay/Character/Character.cs
@@ -87,6 +87,7 @@ public class Character : GameUnit {
 
 	public virtual void OnHit(Character character) {
 		if (!IsDead) {
+			SoundManager.Ins.Play(SoundType.WeaponHit, ref audioSource);
 			IsDead = true;
 			OnDeath();
 		}
