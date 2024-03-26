6821749c2f95587b94b7ff2b719ad7b3188acf2f
trufnbeti <trungboy80@gmail.com>
2024-03-26 09:46:21 +0700

    - fix click button UI gameplay

--- Assets/_Game/Scripts/GamePlay/Skin/Hat.cs
+++ Assets/_Game/Scripts/GamePlay/Skin/Hat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hat : GameUnit
+public class Hat : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
